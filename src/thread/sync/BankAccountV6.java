fd1c5d4ecfb08fb079aa8ec3a55698ece17ce242
dididiri1 <dididiri1@naver.com>
2025-10-09 18:13:35 +0900

    feat: 9. 생산자 소비자 문제1 - 생산자 소비자 문제 - 소개 - 생산자 소비자 문제 - 에제1코드 - 생산자 소비자 문제 - 예제1 분석 생산자 우선 - 생산자 소비자 문제 - 예제1 분석 - 소비자 우선

--- src/thread/sync/BankAccountV6.java
+++ src/thread/sync/BankAccountV6.java
@@ -1,5 +1,6 @@
 package thread.sync;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import static util.MyLogger.log;
@@ -16,14 +17,14 @@ public class BankAccountV6 implements BankAccount {
     @Override
     public boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
-        /*try {
+        try {
             if (!lock.tryLock(500, TimeUnit.MILLISECONDS)) {
                 log("[진입 실패] 이미 처리중인 작업이 있습니다.");
                 return false;
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }*/
+        }
 
         try {
             log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
